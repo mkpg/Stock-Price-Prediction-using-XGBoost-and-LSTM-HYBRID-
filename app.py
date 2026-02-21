76395760db5df721c81cee5448b738fece378b2a
Manikandaprabu <manikandaprabuvk@gmail.com>
2026-02-21 19:11:33 +0530

    v2.0.1
    for RENDER

--- app.py
+++ app.py
@@ -1359,4 +1359,6 @@ def quick_quote(ticker):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_ENV") != "production"
+    app.run(debug=debug, host='0.0.0.0', port=port)
